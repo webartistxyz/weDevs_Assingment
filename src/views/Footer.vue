8a39e6974cc618ce74de82820c9b611cf56e0ccf
Mohiuddin Sadek <tasnif06@gmail.com>
2020-05-24 13:27:08 +0600

    updated footer item text

--- src/views/Footer.vue
+++ src/views/Footer.vue
@@ -1,10 +1,7 @@
 <template>
-  <div
-    class="columns is-vcentered"
-    style="margin-top: .02rem;border-top: 1px solid #EDEDED"
-  >
+  <div class="columns is-vcentered" style="margin-top: .02rem;border-top: 1px solid #EDEDED">
     <div class="column is-3">
-      <span>{{ listLength }} {{ listLength > 1 ? "items" : "item" }} left</span>
+      <span>{{ listLength > 0 ? listLength : 'No'}} {{ listLength > 1 ? "items" : "item" }} left</span>
     </div>
 
     <div class="column is-5">
@@ -12,18 +9,15 @@
         <app-button
           @click="$emit('listCondition', 'all')"
           :class="$parent.listCondition == 'all' ? 'is-active' : ''"
-          >All</app-button
-        >
+        >All</app-button>
         <app-button
           @click="$emit('listCondition', 'active')"
           :class="$parent.listCondition == 'active' ? 'is-active' : ''"
-          >Active</app-button
-        >
+        >Active</app-button>
         <app-button
           @click="$emit('listCondition', 'completed')"
           :class="$parent.listCondition == 'completed' ? 'is-active' : ''"
-          >Completed</app-button
-        >
+        >Completed</app-button>
       </div>
     </div>
     <div class="column is-4 has-text-right" v-if="selected_items.length">
@@ -38,18 +32,18 @@ import AppButton from "../components/AppButton";
 export default {
   name: "Footer",
   components: {
-    AppButton,
+    AppButton
   },
   props: {
     listLength: {
       required: true,
-      type: Number,
+      type: Number
     },
     selected_items: {
       required: false,
-      type: Array,
-    },
-  },
+      type: Array
+    }
+  }
 };
 </script>
 <style scoped>
